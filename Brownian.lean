b7a8c074ca2b27da1590f26d5382d62746ababdf
Dominic Steinitz <dominic@steinitz.org>
2024-10-08 15:57:18 +0100

    Index the RVs with \bbN and add a supply of random numbers

--- Brownian.lean
+++ Brownian.lean
@@ -11,11 +11,29 @@ variable {X : Ω → ℝ} (hX : Measure.map X ℙ = gaussianReal μ v)
 
 variable {_ : MeasurableSpace Ω} {μ : Measure Ω}
 
+def I : Set ℝ := {x | 0 ≤ x ∧ x ≤ 1}
+variable {_ : MeasurableSpace I} {ν : Measure I}
+
 theorem BrowianExistence
     {m : ∀ x, MeasurableSpace ℝ}
     {f : ∀ i, Ω → ℝ} (hf_Indep : iIndepFun m f μ) (h_Normal : Measure.map (f i) μ = gaussianReal 0 1)
     {i j : Ω -> ℝ} (hij : i ≠ j) :
     IndepFun (f i) (f j) μ := sorry
+
+-- FIXME: I may have used the wrong spaces (ℝ instead of I or vice versa)
+def Browian
+    {m : ∀ x, MeasurableSpace ℝ}
+    -- A supply of normally distributed random variables
+    {Z : ∀ i : ℕ, I → ℝ} (hZ_Indep : iIndepFun m Z ν) (h_Normal : Measure.map (Z i) ν = gaussianReal 0 1)
+    -- A supply of random values so that we can sample from the random variables
+    {ω : ℕ -> I} :
+    I -> ℝ -> ℝ :=
+      -- To a first approximation, BM is the linear interpolation of 0 and a random sample from Z 0
+      let F d t := if d == 1
+                   then t * Z 1 (ω 1)
+                   -- Do the ever better approximations once we know how to pattern match and recurse
+                   else pi
+      sorry
 
 def HHaskell {α : Type} [LinearOrderedField α] (n : ℕ) (k : ℕ) (_ : 2 * k - 1 ≤ 2^n -1) (s : α) : ℤ :=
   let k':= 2 * k + 1
